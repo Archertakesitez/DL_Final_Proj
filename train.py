ee31999b7ae68943481321043724456479510c8f
Erchi Zhang <erchizhang@brandeis.edu>
2024-12-13 14:42:40 -0500

    hey

--- train.py
+++ train.py
@@ -133,7 +133,7 @@ def main():
     )
 
     # Initialize model
-    model = JEPAModel(latent_dim=256, use_momentum=True).to(DEVICE)
+    model = JEPAModel(latent_dim=256, momentum=0.99).to(DEVICE)
 
     # Initialize optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
