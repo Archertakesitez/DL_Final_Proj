0c8711cd759c1b314e11bcbfebbcec8580515b54
Erchi Zhang <erchizhang@brandeis.edu>
2024-12-14 00:14:00 -0500

    hey

--- evaluator.py
+++ evaluator.py
@@ -117,7 +117,7 @@ class ProbingEvaluator:
                 init_states = batch.states[:, 0:1]  # BS, 1, C, H, W
                 # print("Evaluator - init_states shape:", init_states.shape)
                 # print("Evaluator - actions shape:", batch.actions.shape)
-                pred_encs = model(states=init_states, actions=batch.actions)
+                pred_encs, _ = model(states=init_states, actions=batch.actions)
                 pred_encs = pred_encs.transpose(0, 1)  # # BS, T, D --> T, BS, D
 
                 # Make sure pred_encs has shape (T, BS, D) at this point
