6ecf5601b7eab8e685e25262b3677634074b5fee
Erchi Zhang <erchizhang@brandeis.edu>
2024-12-14 13:46:29 -0500

    h

--- evaluator.py
+++ evaluator.py
@@ -20,7 +20,7 @@ from normalizer import Normalizer
 @dataclass
 class ProbingConfig(ConfigBase):
     probe_targets: str = "locations"
-    lr: float = 1e-5
+    lr: float = 1e-2
     epochs: int = 20
     schedule: LRSchedule = LRSchedule.Cosine
     sample_timesteps: int = 30
