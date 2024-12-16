8d20282194e550607a1dac1d19d242fc011aeb67
Kenny Wei <kennywei@Kennys-MacBook-Pro.local>
2024-12-16 02:47:32 -0500

    change seed

--- train.py
+++ train.py
@@ -195,7 +195,7 @@ def main():
     )
 
     # Initialize model
-    torch.manual_seed(150)
+    torch.manual_seed(0)
     model = JEPAModel(latent_dim=256, use_momentum=False, momentum=0.5).to(DEVICE)
 
     # Initialize optimizer
